c5247478fea0c2dd2cf867bf1451988e5aac0737
Nicolas <nicolas.glinkin@gmail.com>
2022-04-11 22:15:57 +0300

    users who use bot initialization added

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging, open_xls
 from aiogram import Bot, Dispatcher, executor, types
-from access import bot_token
+from access import bot_token, bot_admin_id
 
 
 # Configure logging
@@ -14,6 +14,12 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['долг', 'долги'])
 async def send_report(message: types.Message):
     await message.answer(open_xls.make_report_text())
+    if message.from_user.id == bot_admin_id:
+        try:
+            await bot.send_message(bot_admin_id, 'пользователь @{} (id:{}) писал боту'.format(message.from_user.username, message.from_user.id))
+        except:
+            await bot.send_message(bot_admin_id, message.from_user.full_name)
+            await bot.send_message(bot_admin_id, 'пользователь {} (id:{}) писал боту'.format(message.from_user.full_name, message.from_user.id))
 
 
 if __name__ == '__main__':
