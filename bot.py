68e8343831fd01c18f956b73f365b54ac2487728
Nicolas <nicolas.glinkin@gmail.com>
2022-04-11 22:47:53 +0300

    users who use bot initialization added 2.1

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['долг', 'долги'])
 async def send_report(message: types.Message):
     await message.answer(open_xls.make_report_text())
-    if str(message.from_user.id) == bot_admin_id:
+    if str(message.from_user.id) != bot_admin_id:
         try:
             await bot.send_message(bot_admin_id, 'пользователь @{} (id:{}) писал боту'.format(message.from_user.username, message.from_user.id))
         except:
