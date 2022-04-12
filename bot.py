636504110f6157f5423ceee6357bea4147199782
Nicolas <Nicolas.gkinkin@gmail.com>
2022-04-12 15:36:46 +0300

    users who use bot initialization added 2.2

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import logging, open_xls
+import logging
+import open_xls
 from aiogram import Bot, Dispatcher, executor, types
 from access import bot_token, bot_admin_id
 
@@ -15,11 +16,11 @@ dp = Dispatcher(bot)
 async def send_report(message: types.Message):
     await message.answer(open_xls.make_report_text())
     if str(message.from_user.id) != bot_admin_id:
-        try:
-            await bot.send_message(bot_admin_id, 'пользователь @{} (id:{}) писал боту'.format(message.from_user.username, message.from_user.id))
-        except:
-            await bot.send_message(bot_admin_id, message.from_user.full_name)
-            await bot.send_message(bot_admin_id, 'пользователь {} (id:{}) писал боту'.format(message.from_user.full_name, message.from_user.id))
+        await bot.send_message(bot_admin_id,
+                               "‼ {} (@{}, id:{}) писал боту".format(
+                                   message.from_user.full_name,
+                                   message.from_user.username,
+                                   message.from_user.id))
 
 
 if __name__ == '__main__':
