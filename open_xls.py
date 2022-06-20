d2c6dfafcc4ac364da83b1333dea4b0bdf0932db
Nicolas <Nicolas.gkinkin@gmail.com>
2022-06-20 18:47:07 +0300

    open_xls optimized

--- open_xls.py
+++ open_xls.py
@@ -7,23 +7,18 @@ def make_report_text():
     dropbox_file_copy.download_sales()
     book = openpyxl.open(local_sales_file_name, read_only=True)
     sheet = book.active
-
-    # создаем список заголовков:
-    columns = {}
-    for now in range(len(sheet[1])):
-        columns[sheet[1][now].value] = now
-
     return_str = ''
+
     for row in sheet:
-        if row[columns['Выплачено?']].value == '-':
+        if row[8].value == '-':                             #проверка на невыплату
             a = '○  *{} - {} руб.*\n{} - {} \n\n'.format(
-                row[9].value,           # комитент
-                row[10].value,          # сумма комитенту
-                row[3].value,           # артикул
-                row[2].value,           # название предмета
+                row[9].value,                               # комитент
+                row[10].value,                              # сумма комитенту
+                row[3].value,                               # артикул
+                row[2].value,                               # название предмета
                 )
             return_str += a
     if return_str == '':
         return 'Нет долгов перед комитентами'
-    # return_str.rstrip()                       не знаю, пригодится ли это. Кажется, уже нет, сохраню на память
+
     return return_str
