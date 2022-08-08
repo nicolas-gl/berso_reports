37ba292016e87e3e4d05fb0d83d873301a9461cd
Nicolas <nicolas.glinkin@gmail.com>
2022-08-09 01:23:26 +0300

    global text modification created

--- open_xls.py
+++ open_xls.py
@@ -8,17 +8,48 @@ def make_report_text():
     book = openpyxl.open(local_sales_file_name, read_only=True)
     sheet = book.active
     return_str = ''
-
+    commitents = set()
+    debts = {}
+    
     for row in sheet:
-        if row[9].value == '-':                             #проверка на невыплату
-            a = '○  *{} - {} руб.*\n{} - {} \n\n'.format(
-                row[10].value,                              # комитент
-                row[11].value,                              # сумма комитенту
-                row[4].value,                               # артикул
-                row[3].value,                               # название предмета
+
+        if row[9].value == '-':
+            com = row[10].value
+            sum = row[11].value
+            ref = row[4].value
+            name = row[3].value
+
+            if com not in commitents:
+                debts[com] = list()
+                commitents.add(com)
+
+            debts[com].append((sum, ref, name))
+
+    for com in sorted(commitents):
+        total_sum = 0  
+        com_debts_str = ''
+        text = ''
+
+        for d in debts[com]:
+            if d[0] != None:
+                total_sum += d[0]
+            else:
+                com_debts_str += '↓↓↓ !ПРОВЕРИТЬ СУММУ! ↓↓↓\n'
+            
+            text += '{} р. - {}, {}\n'.format(
+                d[0],                             # sum
+                d[1],                             # ref
+                d[2]                              # name
                 )
-            return_str += a
+
+        com_debts_str += '○ *{} - {} руб.*\n{}\n\n'.format(
+            com, 
+            total_sum,
+            text
+            )
+
+        return_str += com_debts_str
+
     if return_str == '':
         return 'Нет долгов перед комитентами'
-
     return return_str
