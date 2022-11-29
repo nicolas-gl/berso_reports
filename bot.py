9e75c857fb5484a0f66037dd9799e026464c04b5
Nicolas <nicolas.glinkin@gmail.com>
2022-11-29 19:34:55 +0200

    admin id checking now works with more than 1 user

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['долг', 'долги'])
 async def send_report(message: types.Message):
     await message.answer(open_xls.make_report_text())
-    if str(message.from_user.id) != bot_admin_id:
+    if str(message.from_user.id) not in bot_admin_id.split(' '):
         await bot.send_message(bot_admin_id,
                                "‼ {} (@{}, id:{}) писал боту".format(
                                    message.from_user.full_name,
